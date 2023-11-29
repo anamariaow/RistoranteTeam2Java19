cfff0da89bcd232d502437c2d1975ded8c89ebdb
MattWorkHub <150373956+MattWorkHub@users.noreply.github.com>
2023-11-29 19:08:06 +0100

    Aggiunta main e primi piatti

---
+++ src/PrimiPiatti.java
@@ -0,0 +1,12 @@
+public class PrimiPiatti {
+    public int prezzo;
+    public String nomePiatto;
+    public PrimiPiatti(int prezzo,String nomePiatto) {
+        this.prezzo = prezzo;
+        this.nomePiatto = nomePiatto;
+    }
+    @Override
+    public String toString() {
+        return nomePiatto + " " + prezzo + " Є";
+    }
+}
