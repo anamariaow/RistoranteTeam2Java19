0cffdf972e6ccca626cb4d4c966c56b4f3db0c28
Elian <elianbertola14@gmail.com>
2023-11-30 13:36:35 +0100

    aggiunti secondi piatti al main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         PrimiPiatti assassina = new PrimiPiatti(14, "Spaghetti all'assassina con polpo");
         PrimiPiatti scoglio = new PrimiPiatti(12, "Spaghetti allo scoglio");
 
+        SecondiPiatti orata = new SecondiPiatti(17,"Orata al cartoccio");
+        SecondiPiatti polpo = new SecondiPiatti(12, "Polpo alla luciana");
+        SecondiPiatti gamberoni = new SecondiPiatti(15, "Gamberoni al forno");
+
         System.out.println("Primi piatti :" + "\n\r" + trepoline + "\n\r" + assassina + "\n\r" + scoglio);
+        System.out.println();
+        System.out.println("Secondi piatti :" + "\n\r" + orata + "\n\r" + polpo + "\n\r" + gamberoni);
     }
 }
