6009b2f83b08137d1cef3aa6d6a427749a55eb67
Antonio <taminto.a@gmail.com>
2023-12-04 12:18:52 +0100

    added Antipasti

---
+++ src/Antipasti.java
@@ -0,0 +1,14 @@
+
+    public class Antipasti {
+        public int prezzo;
+        public String nomePiatto;
+        public Antipasti(int prezzo,String nomePiatto) {
+            this.prezzo = prezzo;
+            this.nomePiatto = nomePiatto;
+        }
+        @Override
+        public String toString() {
+            return nomePiatto + " " + prezzo + " Є";
+        }
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Antipasti insaltaDiMare = new Antipasti(10,"insalta di mare");
+        Antipasti cruditesDiMare = new Antipasti(11,"insalta di mare");
+        Antipasti polipettiAffogati = new Antipasti(9,"Polipetti affogati");
+
         PrimiPiatti trepoline = new PrimiPiatti(10, "Trepoline con rape e cozze");
         PrimiPiatti assassina = new PrimiPiatti(14, "Spaghetti all'assassina con polpo");
         PrimiPiatti scoglio = new PrimiPiatti(12, "Spaghetti allo scoglio");
