a50b6d6e1d12f17883a46e244bf94a9d9d14e7db
ET <tacconellielisabetta50@gmail.com>
2024-01-16 12:47:01 +0100

    Risolto conflitto n.2

--- src/prodotti/Portata.java
+++ src/prodotti/Portata.java
@@ -55,11 +55,11 @@ public abstract class Portata {
 
     public void stampaAllergeni() {
         if (!allergeniEnumArrayList.isEmpty()) {
-            System.out.print("(");
+            System.out.print("\u001B[31m" + "(");
             for (AllergeneEnum allergeniEnum : allergeniEnumArrayList) {
-                System.out.print(allergeniEnum.getDescrizione() + ",");
+                System.out.print(allergeniEnum.getDescrizione() + ", ");
             }
-            System.out.println(")");
+            System.out.println(")" + "\u001B[0m");
         }
     }
 }
