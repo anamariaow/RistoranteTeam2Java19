001ec267314e0ed3c714d9f384965319ce43cf1a
MattWorkHub <150373956+MattWorkHub@users.noreply.github.com>
2024-01-24 17:31:36 +0100

    script db fix

--- src/scriptDb.sql
+++ src/scriptDb.sql
@@ -1,11 +1,19 @@
 CREATE DATABASE ristorante_team2
 CREATE TABLE menu(
 id INT PRIMARY KEY AUTO_INCREMENT,
-portata_list VARCHAR(100000) NOT NULL,
-tipo_enum VARCHAR(50) NOT NULL,
+portata_list LONG VARCHAR NOT NULL,
+tipo_enum LONG VARCHAR NOT NULL,
 data_creazione DATETIME NOT NULL
 );
-CREATE TABLE ristorante.primo(
+CREATE TABLE ristorante_team2.primo(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR NOT NULL,
+prezzo DOUBLE NOT NULL,
+allergeniEnumArrayList LONG VARCHAR,
+calorieEnum LONG VARCHAR NOT NULL,
+id_menu FOREIGN KEY
+);
+CREATE TABLE ristorante_team2.antipasto(
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR NOT NULL,
 prezzo DOUBLE NOT NULL,
