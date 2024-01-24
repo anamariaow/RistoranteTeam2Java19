fe09e7b2ea34d977158f9823e9d52ea41c89b75f
anamariaow <new_annemarie@yahoo.com>
2024-01-24 18:12:45 +0100

    aggiunti dessert, vino, drink, soft_drink

--- src/scriptDb.sql
+++ src/scriptDb.sql
@@ -44,3 +44,50 @@ calorie_enum VARCHAR (100),
 id_menu INT,
 FOREIGN KEY (id_menu) REFERENCES menu(id)
 );
+
+CREATE TABLE ristorante_team2.dessert (
+id INT auto_increment NOT NULL PRIMARY KEY,
+nome VARCHAR (255) NOT NULL,
+prezzo DOUBLE NOT NULL,
+lista_allergeni_enum VARCHAR (255),
+calorie_enum VARCHAR (100),
+livello_dolcezza_enum LONG VARCHAR NOT NULL,
+id_menu INT,
+FOREIGN KEY (id_menu) REFERENCES menu(id)
+);
+
+CREATE TABLE ristorante_team2.vino (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+prezzo DOUBLE NOT NULL,
+lista_allergeni_enum VARCHAR NOT NULL,
+calorie_enum VARCHAR NOT NULL,
+tipologia VARCHAR NOT NULL,
+descrizione VARCHAR NOT NULL,
+provenienza VARCHAR NOT NULL,
+anno_produzione INT,
+id_menu INT,
+FOREIGN KEY (id_menu) REFERENCES menu(id)
+);
+
+CREATE TABLE ristorante_team2.drink (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+prezzo DOUBLE NOT NULL,
+lista_allergeni_enum VARCHAR NOT NULL,
+calorie_enum VARCHAR NOT NULL,
+ingredienti VARCHAR NOT NULL,
+id_menu INT,
+FOREIGN KEY (id_menu) REFERENCES menu(id)
+);
+
+CREATE TABLE ristorante_team2.soft_drink (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+prezzo DOUBLE NOT NULL,
+lista_allergeni_enum VARCHAR NOT NULL,
+calorie_enum VARCHAR NOT NULL,
+centilitri VARCHAR NOT NULL,
+id_menu INT,
+FOREIGN KEY (id_menu) REFERENCES menu(id)
+);
